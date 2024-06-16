4f6bd168a734b93fa9f8eed9e5c068f363ed6861
Ruslan <arnov@i.ua>
2024-06-16 16:55:54 +0300

    Step-54

--- js/mobile-burger-menu.js
+++ js/mobile-burger-menu.js
@@ -1,16 +1,14 @@
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector("[data-burger-menu-button-open]"),
-    closeMenuBtn: document.querySelector(".menu-close-btn"),
-    menu: document.querySelector(".header__mob-menu"),
-    body: document.querySelector("body"),
-  };
+  const menuBtnRef = document.querySelector("[data-burger-menu-buttons]");
+  const mobileMenuRef = document.querySelector("[data-burger-menu]");
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
-  }
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
