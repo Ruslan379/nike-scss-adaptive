24a823e3765879a421af6c0fbb1fe8b584371d84
Ruslan <arnov@i.ua>
2024-06-16 18:47:56 +0300

    Step-56

--- js/mobile-burger-menu.js
+++ js/mobile-burger-menu.js
@@ -1,9 +1,14 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-burger-menu-buttons]");
   const mobileMenuRef = document.querySelector("[data-burger-menu]");
+  const mobileMenuItemRef = document.querySelectorAll(".nav-list__item");
 
+  let expanded = false;
+
+  //! Закриття burger-menu по кліку на кнопці 
   menuBtnRef.addEventListener("click", () => {
-    const expanded =
+    console.log("Це клік в burger-menu-buttons");
+    expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
     menuBtnRef.classList.toggle("is-open");
@@ -11,4 +16,26 @@
 
     mobileMenuRef.classList.toggle("is-open");
   });
+
+
+  //! Закриття burger-menu по кліку на елементі списку nav-list
+  mobileMenuItemRef.forEach(function(item) {
+    item.addEventListener('click', function () {
+      console.log("Це клік в nav-list__item");
+
+      expanded =
+        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+      menuBtnRef.classList.toggle("is-open");
+      menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+      mobileMenuRef.classList.toggle("is-open");
+    });
+  });
+
+
+  // function closeBurgerMenu() {
+  //   console.log("Це клік в nav-list__item");
+  // }
+
 })();
